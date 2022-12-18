b823e5a19a0cbbb56f172068da62c48134d3c013
Chris Fredricksen <cmfredricksen@gmail.com>
2022-12-18 07:41:54 -0600

    add fonts, colors, layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../global.css';
+</script>
+
+<svelte:head>
+	<title>CMF DEV</title>
+</svelte:head>
+
+<div class="box">
+	<div class="half" />
+</div>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Color Test</h1>
+
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, totam vero tenetur iusto, maiores
+	illum ducimus, magnam sit quis in officia odio. Eligendi ducimus, sapiente quos eveniet placeat
+	maxime. Error sequi vero omnis, libero quam quas ipsum tempora iusto impedit ab aliquam sint esse
+	veniam necessitatibus fugiat! Ipsum debitis nostrum rerum quam similique qui, officia deserunt eos
+	modi numquam obcaecati perspiciatis, explicabo ut molestiae doloribus quaerat officiis vitae culpa
+	enim quisquam eveniet consequatur. Repellendus numquam beatae harum quasi sunt hic unde optio est
+	molestias, exercitationem ipsa similique cupiditate atque veniam a rerum perspiciatis in aliquam
+	dolorem blanditiis reiciendis? Sapiente, ullam.
+</p>
